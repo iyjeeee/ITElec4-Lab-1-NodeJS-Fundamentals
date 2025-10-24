c92c0fe201856872afe5f9be33b991545a55c5df
Recto, John Andrei D. <iyjeeee.recto@gmail.com>
2025-10-24 19:55:17 +0800

    Authorization and Centralized Error Handling

---
+++ 13-oct-18-ACEH/HelloWorldAPI/index.js
@@ -0,0 +1,28 @@
+// index.js
+import dotenv from 'dotenv';
+dotenv.config();
+
+import express from 'express';
+import postRoutes from './src/routes/post.routes.js';
+import userRoutes from './src/routes/user.routes.js';
+import commentRoutes from './src/routes/comment.routes.js';
+import authRoutes from './src/routes/auth.routes.js';
+import { testConnection } from './src/config/db.js';
+import { errorHandler } from './src/middlewares/errorHandler.middleware.js';
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/posts', postRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/comments', commentRoutes);
+
+app.use(errorHandler);
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+    testConnection(); // Test the database connection on startup
+});
